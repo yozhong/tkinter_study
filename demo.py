8dbc3aaf9bbb737965fbe3f33cab862671a0fbcf
yozhong <yozhong@gmail.com>
2020-01-21 16:43:23 +0800

    [Demo] menu bar

--- demo.py
+++ demo.py
@@ -9,27 +9,54 @@ window.title('My Window')
 # 第3步，設定視窗的大小(長x寬)
 window.geometry('500x300')
 
-# 第4步，在圖形介面上建立 500 * 200 大小的畫布並放置各種元素
-canvas = tk.Canvas(window, bg='green', height=200, width=500)
-# 說明圖片位置，並匯入圖片到畫布上
-image_file = tk.PhotoImage(file='pic.gif')  # 圖片位置（相對路徑，與.py檔案同一資料夾下，也可以用絕對路徑，需要給定圖片具體絕對路徑）
-image = canvas.create_image(250, 0, anchor='n', image=image_file)  # 圖片錨定點（n圖片頂端的中間點位置）放在畫布（250,0）座標處
-# 定義多邊形引數，然後在畫布上畫出指定圖形
-x0, y0, x1, y1 = 100, 100, 150, 150
-line = canvas.create_line(x0 - 50, y0 - 50, x1 - 50, y1 - 50)  # 畫直線
-oval = canvas.create_oval(x0 + 120, y0 + 50, x1 + 120, y1 + 50, fill='yellow')  # 畫圓 用黃色填充
-arc = canvas.create_arc(x0, y0 + 50, x1, y1 + 50, start=0, extent=180)  # 畫扇形 從0度開啟收到180度結束
-rect = canvas.create_rectangle(330, 30, 330 + 20, 30 + 20)  # 畫矩形正方形
-canvas.pack()
+# 第4步，在圖形介面上建立一個標籤用以顯示內容並放置
+label = tk.Label(window, text='      ', bg='green')
+label.pack()
+
+# 第10步，定義一個函式功能，用來代表選單選項的功能，這裡為了操作簡單，定義的功能比較簡單
+counter = 0
 
 
-# 第6步，觸發函式，用來一定指定圖形
-def moveit():
-    canvas.move(rect, 2, 2)  # 移動正方形rect（也可以改成其他圖形名字用以移動一起圖形、元素），按每次（x=2, y=2）步長進行移動
+def do_job():
+    global counter
+    label.config(text='do ' + str(counter))
+    counter += 1
 
 
-# 第5步，定義一個按鈕用來移動指定圖形的在畫布上的位置
-b = tk.Button(window, text='move item', command=moveit).pack()
+# 第5步，建立一個選單欄，這裡我們可以把他理解成一個容器，在視窗的上方
+menubar = tk.Menu(window)
 
-# 第7步，主視窗迴圈顯示
+# 第6步，建立一個File選單項（預設不下拉，下拉內容包括New，Open，Save，Exit功能項）
+filemenu = tk.Menu(menubar, tearoff=0)
+# 將上面定義的空選單命名為File，放在選單欄中，就是裝入那個容器中
+menubar.add_cascade(label='File', menu=filemenu)
+
+# 在File中加入New、Open、Save等小選單，即我們平時看到的下拉選單，每一個小選單對應命令操作。
+filemenu.add_command(label='New', command=do_job)
+filemenu.add_command(label='Open', command=do_job)
+filemenu.add_command(label='Save', command=do_job)
+filemenu.add_separator()  # 新增一條分隔線
+filemenu.add_command(label='Exit', command=window.quit)  # 用tkinter裡面自帶的quit()函式
+
+# 第7步，建立一個Edit選單項（預設不下拉，下拉內容包括Cut，Copy，Paste功能項）
+editmenu = tk.Menu(menubar, tearoff=0)
+# 將上面定義的空選單命名為 Edit，放在選單欄中，就是裝入那個容器中
+menubar.add_cascade(label='Edit', menu=editmenu)
+
+# 同樣的在 Edit 中加入Cut、Copy、Paste等小命令功能單元，如果點選這些單元, 就會觸發do_job的功能
+editmenu.add_command(label='Cut', command=do_job)
+editmenu.add_command(label='Copy', command=do_job)
+editmenu.add_command(label='Paste', command=do_job)
+
+# 第8步，建立第二級選單，即選單項裡面的選單
+submenu = tk.Menu(filemenu)  # 和上面定義選單一樣，不過此處實在File上建立一個空的選單
+filemenu.add_cascade(label='Import', menu=submenu, underline=0)  # 給放入的選單submenu命名為Import
+
+# 第9步，建立第三級選單命令，即選單項裡面的選單項裡面的選單命令
+submenu.add_command(label='Submenu_1', command=do_job)  # 這裡和上面建立原理也一樣，在Import選單項中加入一個小選單命令Submenu_1
+
+# 第11步，建立選單欄完成後，配置讓選單欄menubar顯示出來
+window.config(menu=menubar)
+
+# 第12步，主視窗迴圈顯示
 window.mainloop()
