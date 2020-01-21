8cd945907894ec92ff84fa5e52085d6324f9bf7f
yozhong <yozhong@gmail.com>
2020-01-21 17:08:07 +0800

    [Demo] the pack geometry manager

--- demo.py
+++ demo.py
@@ -9,10 +9,11 @@ window.title('My Window')
 # 第3步，設定視窗的大小(長x寬)
 window.geometry('500x300')
 
-# 第4步，grid 放置方法
-for i in range(3):
-    for j in range(3):
-        tk.Label(window, text=1).grid(row=i, column=j, padx=10, pady=10, ipadx=10, ipady=10)
+# 第4步，pack 放置方法
+tk.Label(window, text='P', fg='red').pack(side='top')     # 上
+tk.Label(window, text='P', fg='red').pack(side='bottom')  # 下
+tk.Label(window, text='P', fg='red').pack(side='left')    # 左
+tk.Label(window, text='P', fg='red').pack(side='right')   # 右
 
 # 第5步，主視窗迴圈顯示
 window.mainloop()
