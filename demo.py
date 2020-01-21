ff0a076eb997bfcca92ae6b9dd548c153c6ad646
yozhong <yozhong@gmail.com>
2020-01-21 16:18:44 +0800

    [Demo] scale bar

--- demo.py
+++ demo.py
@@ -10,29 +10,19 @@ window.title('My Window')
 window.geometry('500x300')
 
 # 第4步，在圖形介面上建立一個標籤label用以顯示並放置
-label = tk.Label(window, bg='yellow', fg='black', width=20, text='empty')
+label = tk.Label(window, bg='green', fg='white', width=20, text='empty')
 label.pack()
 
 
-# 第6步，定義選項觸發函式功能
-def print_selection():
-    if (var1.get() == 1) & (var2.get() == 0):
-        label.config(text='I love only Python ')
-    elif (var1.get() == 0) & (var2.get() == 1):
-        label.config(text='I love only C++')
-    elif (var1.get() == 0) & (var2.get() == 0):
-        label.config(text='I do not love either')
-    else:
-        label.config(text='I love both')
+# 第6步，定義一個觸發函式功能
+def print_selection(v):
+    label.config(text='you have selected ' + v)
 
 
-# 第5步，定義兩個Checkbutton選項並放置
-var1 = tk.IntVar()
-var2 = tk.IntVar()
-c1 = tk.Checkbutton(window, text='Python', variable=var1, onvalue=1, offvalue=0, command=print_selection)
-c1.pack()
-c2 = tk.Checkbutton(window, text='C++', variable=var2, onvalue=1, offvalue=0, command=print_selection)
-c2.pack()
+# 第5步，建立一個尺度滑條，長度200字元，從0開始10結束，以2為刻度，精度為0.01，觸發呼叫print_selection函式
+s = tk.Scale(window, label='try me', from_=0, to=10, orient=tk.HORIZONTAL,
+             length=200, showvalue=0, tickinterval=2, resolution=0.01, command=print_selection)
+s.pack()
 
 # 第7步，主視窗迴圈顯示
 window.mainloop()
